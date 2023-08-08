8291d722e0487ff44ebac59d449ac86ab1930c6a
An-Man <anna.pikkarainen@gmail.com>
2023-08-08 17:10:47 +0300

    a 9.1.2 finished

--- a912/main.cpp
+++ a912/main.cpp
@@ -1,7 +1,7 @@
 #include "template.h"
 #include <iostream>
 #include <type_traits>
-
+#include <typeinfo>
 
 int sum(int a, int b) {
     return a + b;
@@ -13,16 +13,20 @@ void example2() {
 
 int main() {
 
-Caller c(sum);
-Caller d(example2);
+    Caller c(sum);
+    Caller d(example2);
 
-decltype(c)::return_type num = 6; // just a check, int can be assigned
-    
-std::cout << std::boolalpha;
-std::cout << std::is_void_v<decltype(c)::return_type> << '\n';
-std::cout << std::is_void_v<decltype(d)::return_type> << '\n';
+    decltype(c)::return_type num = 6; // just a check, int can be assigned
+        
+    std::cout << std::boolalpha;
+    std::cout << std::is_void_v<decltype(c)::return_type> << '\n';  // false
+    std::cout << std::is_void_v<decltype(d)::return_type> << '\n';  // true
 
-std::cout << decltype(c)::arg_count << '\n';    // prints 2
-std::cout << decltype(d)::arg_count << '\n';    // prints 0
+    // next 2 lines from Karri :D
+    std::cout << "Return type: " << typeid(typename decltype(c)::return_type).name() << '\n';
+    std::cout << "Return type: " << typeid(typename decltype(d)::return_type).name() << '\n';
+
+    std::cout << decltype(c)::arg_count << '\n';    // prints 2
+    std::cout << decltype(d)::arg_count << '\n';    // prints 0
 
 }
